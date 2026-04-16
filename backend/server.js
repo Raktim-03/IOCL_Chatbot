073e87769239201cdcb09391244ae2b184a3a6b0
Raktim Ranjan Handique <raktimhandique1010@gmail.com>
2026-04-16 13:19:43 +0530

    updated for single redner

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Node backend running");
+// API test route
+app.get("/api", (req, res) => {
+  res.send("API is running");
 });
 
-const PORT = 5000;
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Backend running on port ${PORT}`);
 });
